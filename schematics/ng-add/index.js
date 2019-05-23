56ce34786f46603a1cfb2fc0b3ae7697e0a73d1d
Benjamin Dobler <benz@richapps.de>
2019-05-23 22:47:25 +0200

    feat: add ng add schematic, fix ng generate

---
+++ schematics/ng-add/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const schematics_1 = require("@angular-devkit/schematics");
+function default_1(options) {
+    return (tree, _context) => {
+        return schematics_1.chain([])(tree, _context);
+    };
+}
+exports.default = default_1;
